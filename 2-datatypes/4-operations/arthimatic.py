6710c2e757ecf1e9f172c352db01fb42ba063afa
ravisesham3 <ravindrasesham4@gmail.com>
2024-12-20 19:57:43 +0530

    ravi

--- 2-datatypes/4-operations/arthimatic.py
+++
@@ -1,9 +0,0 @@
-x = 9
-y = 12
-z = 40
-print(x+y)
-print(x-y)
-print(x*y)
-print(x/y)
-print(x%y)
-print(x*y*z)
---
+++ 4-Operators/arthimatic.py
@@ -0,0 +1,8 @@
+x = 5
+y = 15
+z = 8.9
+print(x+y+z)
+print(x*y*z)
+print(y-z)
+print(y/x)
+print(y%x)
