6136cc438b11051cec6d21e5519291ea90255976
ravisesham3 <ravindrasesham4@gmail.com>
2025-01-03 13:01:21 +0530

    ravi

--- 6-Conditional statements/if conditions.py
+++ 6-Conditional statements/if conditions.py
@@ -3,7 +3,7 @@ z = x % 2
 if z==0:
     print("The value is Even")      #if condition is use in case of true value
     if x > 10:                      #else is used in case of if fails
-        print("Great value")
+        print("Great value")        #one statement inside another statement is called nested if
     else:
         print("Not a great value")
 else:
---
+++ 7-Loop statements/while.py
@@ -0,0 +1,5 @@
+i = 1                   #initialization
+                        #In this case condition is out of range then there is nothing to be print
+while i<=5:                 #condition
+    print("Ravi is a good boy")
+    i = i+1                 #increment/Decrement
