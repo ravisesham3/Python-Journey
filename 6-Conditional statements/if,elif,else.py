e4b31ae8dda45ea832f6a4d8bd07acabbd121225
ravisesham3 <ravindrasesham4@gmail.com>
2025-01-03 12:45:57 +0530

    ravi

---
+++ 6-Conditional statements/if,elif,else.py
@@ -0,0 +1,17 @@
+x = 1
+
+if x==1:
+    print("one")        #if condition is true then it will be print
+                        #in case elif is true it will be print
+elif x==2:    
+    print("two")    
+                        #The value is out of range then we take the else statement    print("second")
+elif x==3:
+    print("third")
+
+elif x==4:
+    print("four")
+
+
+else:
+    print("wrong statement")
