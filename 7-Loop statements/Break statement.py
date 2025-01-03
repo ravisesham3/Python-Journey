2faff5e4d77518395df4f41b310ea485cffce863
ravisesham3 <ravindrasesham4@gmail.com>
2025-01-03 19:08:55 +0530

    ravi

---
+++ 7-Loop statements/Break statement.py
@@ -0,0 +1,12 @@
+av =5
+x = int(input("Enter how many chocolates do you want!"))
+
+i = 3
+while i <= x:
+    if i>av:
+        print("out of stock")
+        break
+    print("chocolates")
+    i+=1
+
+print("bye")
