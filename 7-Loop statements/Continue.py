ca84a5f13e5d0e566f55118d4ecf1d47e6c3a149
ravisesham3 <ravindrasesham4@gmail.com>
2025-01-03 19:22:51 +0530

    ravi

---
+++ 7-Loop statements/Continue.py
@@ -0,0 +1,7 @@
+for i in range(1,101):   #It will be printed as 1 to 100 numbers
+
+    if i%3==0 or i%5==0:    #Divisible of 3 and 5 are not printed
+         continue           #If the continue function is used to skip the specific values
+    print(i)
+
+print("Bye")
