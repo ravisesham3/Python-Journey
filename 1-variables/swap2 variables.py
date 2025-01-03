584dd611168e1a49b61bc86a35bc30006f77e31f
ravisesham3 <ravindrasesham4@gmail.com>
2025-01-03 11:31:56 +0530

    ravi

---
+++ 1-variables/swap2 variables.py
@@ -0,0 +1,11 @@
+a = 5
+b = 6
+
+temp = a        #In this purpose we use a third variable temp
+a = b
+b = temp
+print(a)
+print(b)
+
+
+
---
+++ 1-variables/swap2 variables2.py
@@ -0,0 +1,8 @@
+a = 5
+b = 6
+
+a = a + b # 11       #In this purpose we use formulas and it works
+b = a - b # 5       # In this use see an extra bits in 11 ,to overcome this we use ^ symbol 
+a = a - b # 6
+print(a)
+print(b)
