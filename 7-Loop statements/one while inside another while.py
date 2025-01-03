7bba522672eb7f46f9adb4b5973cb74e90d11c62
ravisesham3 <ravindrasesham4@gmail.com>
2025-01-03 13:17:04 +0530

    ravi

---
+++ 7-Loop statements/one while inside another while.py
@@ -0,0 +1,12 @@
+i = 1
+
+while i<=5:                     
+    print("ravi",end="")
+    j = 1
+    while j<=2:
+        print("ram",end="")
+        j=j+1                       #first increment and then i will be incrementing
+                                    #j finish then out of loop and i incremented
+
+    i=i+1
+    print()
